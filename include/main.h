3a19d2b74317763e1f9446d1130749bdcdcbae08
John Van Enk <vanenkj@gmail.com>
2011-10-18 11:33:09 -0300

    'overlow' -> 'overflow'

--- include/main.h
+++ include/main.h
@@ -3,7 +3,7 @@
 
 /* Assuming a clock rate of 16Mhz and a CLK/64 prescaler,
  * we will see 250 ticks per millisecond. If we want to
- * have the timer overlow every millisecond, we need to
+ * have the timer overflow every millisecond, we need to
  * initialize the counter to 5 after each tick. */
 #define TIMER_RESET_VAL 5
 
